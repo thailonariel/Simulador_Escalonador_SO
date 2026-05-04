d463c218f5e7f16fb2ede6c327bbea4829f2f9f4
Thailon Ariel Fritsch <thailonarielf@gmail.com>
2026-05-04 19:06:26 -0300

    Alterações de comentários

--- repositorio.py
+++ repositorio.py
@@ -2,13 +2,13 @@ from processo import Processo
 
 class RepositorioProcessos:
     def __init__(self):
-        # Lista que armazena os objetos do tipo Processo [cite: 39]
+        # Lista que armazena os objetos do tipo Processo
         self.processos = []
 
     def adicionar(self, p):
-        # Adiciona um novo processo ao repositório [cite: 40]
+        # Adiciona um novo processo ao repositório
         self.processos.append(p)
 
     def listar(self):
-        # Retorna a lista completa para o escalonador [cite: 41, 42]
+        # Retorna a lista completa para o escalonador
         return self.processos
